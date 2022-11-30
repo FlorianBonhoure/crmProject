6a82c4869bd913c2f24af38cd0356675d30bafd9
Florian Bonhoure <florian.b47@hotmail.fr>
2022-11-30 15:09:53 +0100

    add view clients list with thymeleaf

---
+++ src/main/java/com/javapoe/crmProject/controllermvc/ClientMvcController.java
@@ -0,0 +1,27 @@
+package com.javapoe.crmProject.controllermvc;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.javapoe.crmProject.model.Client;
+import com.javapoe.crmProject.service.ClientDirectory;
+
+@Controller
+public class ClientMvcController {
+	
+	@Autowired
+	ClientDirectory clientDirectory;
+	
+	@GetMapping("clients")
+	public ModelAndView listClients() {
+		ModelAndView view = new ModelAndView("affichage-clients.html");
+		List<Client> clients = clientDirectory.getAllClients();
+		view.addObject("clients",clients);
+		return view;
+	}
+
+}
